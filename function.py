85d09ab188c5337a6f18dfdcfe3a24ecb86a8eec
Maksim <maskimangerman@gmail.com>
2022-04-14 19:17:06 +0300

    удаление лишенго+ обучение рабоиты с git'ом)

--- function.py
+++ function.py
@@ -12,29 +12,3 @@ def ran_pin():
     print('It\'s Your password number:', a)
     return a
 
-# a = random.randint(1, 9)
-# b = random.randint(1, 9)
-# c = random.randint(1, 9)
-# d = random.randint(1, 9)
-# e = random.randint(1, 9)
-# f = random.randint(1, 9)
-# g = random.randint(1, 9)
-# print(a, b, c ,d, e, f, g)
-# def ran ():
-#     a = random.randint(1, 9)
-#     b = random.randint(1, 9)
-#     c = random.randint(1, 9)
-#     d = random.randint(1, 9)
-#     e = random.randint(1, 9)
-#     print(a, b, c, d, e)
-#     return a, b, c, d, e
-#
-#
-#
-# def ran_pin():
-#     a = random.randint(1, 9)
-#     b = random.randint(1, 9)
-#     c = random.randint(1, 9)
-#     d = random.randint(1, 9)
-#     print(a, b, c, d)
-#     return a, b, c, d
